58827fee54460c7850385471bc4d8102ebdf831c
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-04-27 02:40:16 +0400

    commit it

---
+++ 021-dealers-task/src/components/section3/Section3.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Section3.scss'
+
+const Section3 = () => {
+  return (
+    <>
+    <div className="wrapper3">
+       <div className="leftSide">
+        <div className="imgBox">
+            <img src="https://preview.colorlib.com/theme/dealers/images/model_woman_1.png.webp" alt="" />
+        </div>
+       </div>
+       <div className="rightSide">
+      <div className="text">
+      <p>#New Summer Collection 2019</p>
+        <h2>JACKET</h2>
+        <button className='btn'>Shop now</button>
+       </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Section3
