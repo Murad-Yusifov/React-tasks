c4d30865cb53ec7f2f3ab1a3265a15b57a0ada13
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-05-07 23:29:53 +0400

    commit it

---
+++ 024-eComerce/frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="footer">    
+    <p>Copyright ©2025 All rights reserved | This template is made with | by Colorlib</p>
+    </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ 024-eComerce/frontend/src/components/sections/section7/Section7.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Section7.scss'
+
+const Section7 = () => {
+  return (
+    <>
+    <div className="wrapper7">
+        <div className="contain">
+            <div className="imgBox">
+                <img src="https://preview.colorlib.com/theme/shop/img/br1.png.webp" alt="" />
+            </div>
+            <div className="imgBox">
+                <img src="https://preview.colorlib.com/theme/shop/img/br2.png.webp" alt="" />
+            </div>
+            <div className="imgBox">
+                <img src="https://preview.colorlib.com/theme/shop/img/br3.png.webp" alt="" />
+            </div>
+            <div className="imgBox">
+                <img src="https://preview.colorlib.com/theme/shop/img/br4.png.webp" alt="" />
+            </div>
+            <div className="imgBox">
+                <img src="https://preview.colorlib.com/theme/shop/img/br5.png.webp" alt="" />
+            </div>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Section7
