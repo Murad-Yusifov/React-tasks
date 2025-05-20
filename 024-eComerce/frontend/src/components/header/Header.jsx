a8a18035211394b728c8dd482755ed4df72b7590
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-05-20 05:20:36 +0400

    mshop task is working!

--- 024-eComerce/frontend/src/components/header/Header.jsx
+++ 024-eComerce/frontend/src/components/header/Header.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import './Header.scss';
 import { Link } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBasketThunk } from '../../redux/reducers/basketSlice';
 
 const Header = () => {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const wishList = JSON.parse(localStorage.getItem("wish")) || [];
-    const totalItems = wishList.reduce((acc, item) => acc + (item.count || 1), 0);
-    setCount(totalItems);
-  }, []);
+  const dispatch =useDispatch()
+
+  const data = useSelector(state=>state.basket.basket)
+  const loading = useSelector(state=>state.basket.loading)
+
+  useEffect(()=>{
+    
+  dispatch(getBasketThunk())
+  },[])
 
   return (
     <div className="wrapper">
@@ -22,8 +27,8 @@ const Header = () => {
           <li><Link className='link' to='/category'>Category</Link></li>
           <li><Link className='link' to='/men'>Men</Link></li>
           <li><Link className='link' to='/women'>Women</Link></li>
-          <li><Link className='link' to='/wish'>Wish List ({count})</Link></li>
-          <li><Link className='link' to='/admin'>Admin</Link></li>
+          <li><Link className='link' to='/wish'>Wish List</Link></li>
+          <li><Link className='link' to='/admin'>Admin  ({loading ? '...' : data.length})</Link></li>
         </ul>
         <div className="burger">
           <span></span>
