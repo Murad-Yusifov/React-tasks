90829fca2e31135a386b6bad8b2a06546b9bbe40
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-05-29 06:49:27 +0400

    commit it

---
+++ 02-games/01-flappybird/flappyScript.js
@@ -0,0 +1,14 @@
+let board
+let board_w=400
+let board_h=600
+let context
+
+let slap_sfx = new Audio('assets/slap.wav')
+let woosh_sfx  = new Audio('assets/woosh.wav')
+let score_sfx  = new Audio('assets/score.wav')
+
+let player_w =57
+let player_h =58
+let player_x =172
+let player_y =300
+let velocity =0
